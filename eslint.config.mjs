7de34c48a6d0b997b15eef9dc53eb6c872967ba8
Jaime Pineda <jpin730@pm.me>
2025-09-19 14:48:00 -0600

    chore: :wrench: update ESLint and Prettier configurations, add tailwind css plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import prettier from 'eslint-plugin-prettier/recommended'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -10,10 +10,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
+/** @type {import('eslint').Linter.Config[]} */
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  {
+    rules: {
+      'no-console': 'warn',
+    },
   },
   prettier,
 ]
@@ -22,3 +28,5 @@ export default eslintConfig
 
 // To install the required packages, run:
 // pnpm add eslint eslint-config-next @eslint/eslintrc
+// For extentions support, run:
+// pnpm add -D @next/eslint-plugin-next eslint-plugin-react-hooks
