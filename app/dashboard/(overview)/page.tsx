33d5c746828efe62bccb8c47a95b34da49363014
Jaime Pineda <jpin730@pm.me>
2025-10-03 17:22:28 -0600

    refactor: :zap: implement suspense component and skeleton in dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,12 @@
+import { fetchCardData } from '@/app/lib/data'
+import { Card } from '@/app/ui/dashboard/cards'
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
 import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import { lusitana } from '@/app/ui/fonts'
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data'
-import { Card } from '../ui/dashboard/cards'
-import LatestInvoices from '../ui/dashboard/latest-invoices'
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons'
+import { Suspense } from 'react'
 
 export default async function Page() {
-  const revenue = await fetchRevenue()
-  const latestInvoices = await fetchLatestInvoices()
   const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } =
     await fetchCardData()
 
@@ -20,8 +20,12 @@ export default async function Page() {
         <Card title="Total Customers" value={numberOfCustomers} type="customers" />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   )
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,14 +1,12 @@
-import { LatestInvoice } from '@/app/lib/definitions'
+import { fetchLatestInvoices } from '@/app/lib/data'
 import { lusitana } from '@/app/ui/fonts'
 import { ArrowPathIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
 import Image from 'next/image'
 
-interface Props {
-  latestInvoices: LatestInvoice[]
-}
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices()
 
-export default async function LatestInvoices({ latestInvoices }: Readonly<Props>) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Latest Invoices</h2>
