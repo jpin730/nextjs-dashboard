32e42a643685398fee987f1da51e4b5cf2d01007
Jaime Pineda <jpin730@pm.me>
2025-10-07 09:55:45 -0600

    feat: :sparkles: create Card and CardWrapper components for dashboard metrics

--- app/ui/dashboard/card.tsx
+++ app/ui/dashboard/card.tsx
@@ -1,5 +1,5 @@
-import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from '@heroicons/react/24/outline'
 import { lusitana } from '@/app/ui/fonts'
+import { BanknotesIcon, ClockIcon, InboxIcon, UserGroupIcon } from '@heroicons/react/24/outline'
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -8,32 +8,13 @@ const iconMap = {
   invoices: InboxIcon,
 }
 
-export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
-    </>
-  )
-}
-
-export function Card({
-  title,
-  value,
-  type,
-}: {
+interface Props {
   title: string
   value: number | string
   type: 'invoices' | 'customers' | 'pending' | 'collected'
-}) {
+}
+
+export function Card({ title, value, type }: Readonly<Props>) {
   const Icon = iconMap[type]
 
   return (
