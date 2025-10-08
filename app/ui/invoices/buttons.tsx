0637d4c3ac84696f9ea93cb0c0bc563f16b313ef
Jaime Pineda <jpin730@pm.me>
2025-10-08 14:45:05 -0600

    feat: :sparkles: implement invoice editing functionality with form and breadcrumbs

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,10 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 
+interface Props {
+  id: string
+}
+
 export function CreateInvoice() {
   return (
     <Link
@@ -12,21 +16,22 @@ export function CreateInvoice() {
   )
 }
 
-export function UpdateInvoice({ id }: { id: string }) {
+export function UpdateInvoice({ id }: Readonly<Props>) {
   return (
-    <Link href="/dashboard/invoices" className="rounded-md border p-2 hover:bg-gray-100">
+    <Link
+      href={`/dashboard/invoices/${id}/edit`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
       <PencilIcon className="w-5" />
     </Link>
   )
 }
 
-export function DeleteInvoice({ id }: { id: string }) {
+export function DeleteInvoice({ id }: Readonly<Props>) {
   return (
-    <>
-      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-5" />
-      </button>
-    </>
+    <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+      <span className="sr-only">Delete</span>
+      <TrashIcon className="w-5" />
+    </button>
   )
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { updateInvoice } from '@/app/lib/actions'
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions'
+import { Button } from '@/app/ui/button'
 import {
   CheckIcon,
   ClockIcon,
@@ -8,17 +10,17 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-import { Button } from '@/app/ui/button'
 
-export default function EditInvoiceForm({
-  invoice,
-  customers,
-}: {
+interface Props {
   invoice: InvoiceForm
   customers: CustomerField[]
-}) {
+}
+
+export default function EditInvoiceForm({ invoice, customers }: Readonly<Props>) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
