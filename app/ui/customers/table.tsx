ffe03159f6af36b8f35982dee11b957ff98edc83
Jaime Pineda <jpin730@pm.me>
2025-10-14 18:18:48 -0600

    refactor: :recycle: reorganize imports and improve type definition for CustomersTable component

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
+import { FormattedCustomersTable } from '@/app/lib/definitions'
 import { lusitana } from '@/app/ui/fonts'
 import Search from '@/app/ui/search'
-import { CustomersTableType, FormattedCustomersTable } from '@/app/lib/definitions'
+import Image from 'next/image'
 
-export default async function CustomersTable({
-  customers,
-}: {
+interface Props {
   customers: FormattedCustomersTable[]
-}) {
+}
+
+export default async function CustomersTable({ customers }: Readonly<Props>) {
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>Customers</h1>
